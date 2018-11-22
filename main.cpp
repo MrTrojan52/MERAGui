71fed4817d6b4e881b45091ef18f3d92cb5f0986
MrTrojan52 <kirill_131097@mail.ru>
2018-11-23 00:27:11 +0300

    Combobox stylisation

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     qRegisterMetaType<string>("string");
     Q_INIT_RESOURCE(resources);
-    a.setFont(QFont("Roboto", 11, QFont::Normal));
+    QFile styleFile(":/style/assets/ControlStyles.qss");
+    styleFile.open(QFile::ReadOnly);
+    a.setFont(QFont("Roboto", 11, QFont::Normal));    
+    a.setStyleSheet(QLatin1String(styleFile.readAll()));
+    styleFile.close();
     Dialog w;
     w.show();
 
